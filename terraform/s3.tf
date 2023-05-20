23e272cf343990d684761d92604f6437e9a3f624
Will Weaver <monkey@buildingbananas.com>
2023-05-20 18:46:07 -0400

    debugging terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -47,24 +47,24 @@ resource "aws_s3_bucket_acl" "public_bucket" {
 #   acl    = "public-read"
 # }
 
-# resource "aws_s3_bucket_policy" "public_bucket_policy" {
-#   bucket = aws_s3_bucket.public_bucket.id
-#   policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Id": "PublicReadAllow",
-#   "Statement": [
-#     {
-#       "Sid": "PublicReadGetObject",
-#       "Effect": "Allow",
-#       "Principal": "*",
-#       "Action": "s3:GetObject",
-#       "Resource": "arn:aws:s3:::${var.environment}${var.domain_name}/*"
-#     }
-#   ]
-# }
-# POLICY
-# }
+resource "aws_s3_bucket_policy" "public_bucket_policy" {
+  bucket = aws_s3_bucket.public_bucket.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "PublicReadAllow",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.environment}${var.domain_name}/*"
+    }
+  ]
+}
+POLICY
+}
 
 resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.public_bucket.bucket
