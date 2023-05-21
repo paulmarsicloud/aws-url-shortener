8ff1733c4dbcbf739fbd894459d2482b1e0ac6b3
Will Weaver <monkey@buildingbananas.com>
2023-05-20 21:23:08 -0400

    cleanup terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -42,11 +42,6 @@ resource "aws_s3_bucket_acl" "public_bucket" {
   acl    = "public-read"
 }
 
-# resource "aws_s3_bucket_acl" "public_bucket_acl" {
-#   bucket = aws_s3_bucket.public_bucket.id
-#   acl    = "public-read"
-# }
-
 resource "aws_s3_bucket_policy" "public_bucket_policy" {
   bucket = aws_s3_bucket.public_bucket.id
   policy = <<POLICY
